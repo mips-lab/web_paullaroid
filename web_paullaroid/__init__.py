c5fa1e38fecaeed6bfab28de2ddef215d02978db
Voileux <simon.recher@resel.fr>
2016-04-14 22:50:49 +0200

    fix

--- web_paullaroid/__init__.py
+++ web_paullaroid/__init__.py
@@ -5,6 +5,9 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
+
+ #   config.registry.settings = settings
+
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
--- web_paullaroid/views.py
+++ web_paullaroid/views.py
@@ -4,10 +4,10 @@ import os.path
 
 @view_config(route_name='home', renderer='templates/home.pt')
 def home_view(request):
-    events_gen = glob.iglob('../data/*') #it's a gen 
+    events_gen = glob.iglob(request.registry.settings.get('directory')) #it's a gen 
     events = []
     for event in events_gen: 
-        events.append = os.path.basename(event)
+        events.append(os.path.basename(event))
     
     return {'project': 'web_paullaroid', 'events': events}
 
