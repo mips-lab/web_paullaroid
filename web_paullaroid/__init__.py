f5c2f39892d3ec10bf00a5df661b3d6bb3a30399
Voileux <simon.recher@resel.fr>
2016-04-14 21:51:18 +0200

    Add event and image route

--- web_paullaroid/__init__.py
+++ web_paullaroid/__init__.py
@@ -8,5 +8,7 @@ def main(global_config, **settings):
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('event', '/{event}/')
+    config.add_route('image', '/{event}/{image}/')
     config.scan()
     return config.make_wsgi_app()
