d53acc210fa43fe5b40fc1539ffabde8c049a35e
Voileux <simon.recher@resel.fr>
2016-04-14 22:57:02 +0200

    because with good path is better

--- web_paullaroid/views.py
+++ web_paullaroid/views.py
@@ -4,7 +4,7 @@ import os.path
 
 @view_config(route_name='home', renderer='templates/home.pt')
 def home_view(request):
-    events_gen = glob.iglob(request.registry.settings.get('directory')) #it's a gen 
+    events_gen = glob.iglob(os.path.join(request.registry.settings.get('directory'),'*')) #it's a gen 
     events = []
     for event in events_gen: 
         events.append(os.path.basename(event))
