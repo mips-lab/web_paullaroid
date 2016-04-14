91ff0fd14cf6a7ee3b98f287527f9388693db79d
Voileux <simon.recher@resel.fr>
2016-04-14 23:33:45 +0200

    super, we are paulla

--- web_paullaroid/views.py
+++ web_paullaroid/views.py
@@ -1,6 +1,10 @@
-from pyramid.view import view_config
 import glob
 import os.path
+
+from pyramid.view import view_config
+from pyramid.response import FileResponse
+
+
 
 @view_config(route_name='home', renderer='templates/home.pt')
 def home_view(request):
@@ -10,12 +14,6 @@ def home_view(request):
         events.append(os.path.basename(event))
     
     return {'project': 'web_paullaroid', 'events': events}
-
-
-@view_config(route_name='image', renderer='templates/image.pt')
-def image_view(request):
-    return {'image': request.matchdict.get('image'), 'event':
-    request.matchdict.get('event')}
 
 
 @view_config(route_name='event', renderer='templates/event.pt')
@@ -28,3 +26,20 @@ def event_view(request):
         images.append(os.path.basename(image))
     
     return {'event_name': event_name, 'images' : images}
+
+
+@view_config(route_name='image', renderer='templates/image.pt')
+def image_view(request):
+    return {'image': request.matchdict.get('image'), 'event':
+            request.matchdict.get('event')}
+
+
+@view_config(route_name='image_raw')
+def image_raw_view(request):
+    event_name = request.matchdict['event'] 
+    image_name = request.matchdict['image'] 
+    path = os.path.join(request.registry.settings.get('directory'),event_name,
+image_name)
+    response = FileResponse(path, request=request)
+#    reponse.content_disposition = u'attachment; filename="%s"' % image_name
+    return response
