4d19afcf74fe6b33082a7dc8d045263e127f91ee
Cyprien Le Pannérer <cyplp@free.fr>
2016-06-07 19:12:06 +0200

    prefer True to 1

--- web_paullaroid/views.py
+++ web_paullaroid/views.py
@@ -12,53 +12,50 @@ def home_view(request):
     ''' home view
         parse dir in data, each dir is an event
     '''
-    events_gen = glob.iglob(os.path.join(request.registry.settings.get('directory'),'*')) #it's a gen 
+    events_gen = glob.iglob(os.path.join(request.registry.settings.get('directory'),'*')) #it's a gen
     events = []
-    for event in events_gen: 
+    for event in events_gen:
         events.append(os.path.basename(event))
-    
+
     return {'project': 'web_paullaroid', 'title': 'PauLLAroid' , 'events': events}
 
 
 @view_config(route_name='event', renderer='templates/event.pt')
 def event_view(request):
-    ''' event view 
-        first visit just see only 10 newest pics, 
-        you can select date, to find specific pic 
+    ''' event view
+        first visit just see only 10 newest pics,
+        you can select date, to find specific pic
     '''
     date_post = request.GET.get('date_pict')
     event_name = request.matchdict['event']
 
     images_gen = glob.iglob(os.path.join(request.registry.settings.get('directory'), event_name, '*.jpg'))  # it's a gen
     images = []
-    for image in images_gen: 
+
+    for image in images_gen:
         images.append(os.path.basename(image))
-    images.sort(reverse=1)
+
+    images.sort(reverse=True)
 
     dates_tmp= {image.split('_')[0] for image in images}
 
-    dates = [datetime.date(int(date.split('-')[0]), int(date.split('-')[1]),
-                            int(date.split('-')[2])) for date in dates_tmp]
-    dates.sort(reverse=1)
+    dates = [datetime.date(int(date.split('-')[0]),
+                           int(date.split('-')[1]),
+                           int(date.split('-')[2])) for date in dates_tmp]
+    dates.sort(reverse=True)
 
     if date_post:
         images = [image for image in images if image.startswith(date_post)]
 
-        date_post_obj = datetime.date(int(date_post.split('-')[0]), int(date_post.split('-')[1]),
-                            int(date_post.split('-')[2]))
-        selected_date =  dates.pop(dates.index(date_post_obj))
     else:
         images = images[:10]
-        selected_date = dates.pop(0)
-    
-    import pdb; pdb.set_trace()
-    return {'title' : event_name , 'event_name': event_name, 'images' : images,
-'dates' : dates, 'selected_date': selected_date }
+
+    return {'title' : event_name , 'event_name': event_name, 'images' : images, 'dates' : dates}
 
 
 @view_config(route_name='image', renderer='templates/image.pt')
 def image_view(request):
-    ''' image view 
+    ''' image view
         see image with 100% of screen size
     '''
     return {'title' : request.matchdict.get('event') +
@@ -71,8 +68,8 @@ def image_thumb_view(request):
     ''' image thumb view
         see thumb of each picture (with  .thumbnail extension)
     '''
-    event_name = request.matchdict['event'] 
-    image_name = request.matchdict['image'] 
+    event_name = request.matchdict['event']
+    image_name = request.matchdict['image']
     path = os.path.join(request.registry.settings.get('directory'),event_name,
 image_name+".thumbnail")
     response = FileResponse(path, request=request)
@@ -83,10 +80,10 @@ image_name+".thumbnail")
 @view_config(route_name='image_raw')
 def image_raw_view(request):
     ''' image raw view
-        see raw of each picture 
+        see raw of each picture
     '''
-    event_name = request.matchdict['event'] 
-    image_name = request.matchdict['image'] 
+    event_name = request.matchdict['event']
+    image_name = request.matchdict['image']
     path = os.path.join(request.registry.settings.get('directory'),event_name,
 image_name)
     response = FileResponse(path, request=request)
