9e68f38435c9586e88acb8dcab6c8a7c88858b75
Cyprien Le Pannérer <cyplp@free.fr>
2016-11-03 18:02:35 +0100

    add doc._id for next and prev

--- web_paullaroid/data/images.js
+++ web_paullaroid/data/images.js
@@ -3,7 +3,7 @@
    "language": "javascript",
    "views": {
        "all": {
-           "map": "function(doc) {\n  if (doc.type_doc == 'image')\n\t{\n\t  var tmp = doc._id.split('_');\n  \t  emit([doc.event_id, tmp[0]], doc);\n}\n}",
+           "map": "function(doc) {\n  if (doc.type_doc == 'image')\n\t{\n\t  var tmp = doc._id.split('_');\n  \t  emit([doc.event_id, tmp[0], doc._id], doc);\n}\n}",
            "reduce": "function(keys, values, rereduce)\n{\n   var result = {nb: 0};\n   if (rereduce){\n\tfor(var i=0; i < values.length; i++) {\n\t  result.nb += values[i].nb;\n\t }\n\treturn result;\n     }\n   result.nb = values.length;\n   return result;\n}"
        }
    }
